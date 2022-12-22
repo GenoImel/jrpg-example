919b9aaf1fe98721547491e16eaeb3edf090a84f
genoimel <genoimel@gmail.com>
2022-12-21 23:21:56 -0500

    Starting to work on initializing battlescreen from scratch.

---
+++ Assets/Scripts/BattleSystem/BattlescreenInitializer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace JRPG.BattleSystem
+{
+    public class BattlescreenInitializer : MonoBehaviour
+    {
+        void Start()
+        {
+            //TO DO
+            //Place player characters on player side of battleground
+
+            //Place enemy characters on enemy side of battleground
+            
+            //And now that I've started drafting this out in my head I've realized that
+            //this script is already responsible for too many things and isn't modular enough.
+            
+            //I will instead make player chars, enemies, and the character info panel responsible for themselves
+        }
+    }
+}
