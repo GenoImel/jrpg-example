47d69c3186f108983d30fbc6cb3b1081edf7cf6f
genoimel <genoimel@gmail.com>
2023-02-13 23:10:49 -0500

    Refactoring BattleCommandsPanel prefab, began input implementation.

--- Assets/Scripts/BattleSystem/UI/BattleOptionsController.cs
+++ Assets/Scripts/BattleSystem/UI/BattleOptionsController.cs
@@ -1,25 +1,43 @@
+using System;
+using JRPG.Input;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 namespace JRPG.BattleSystem.UI
 {
     internal sealed class BattleOptionsController : MonoBehaviour
     {
-        [Header("Panels")] 
-        [SerializeField] private GameObject attackPanel;
-
-        [SerializeField] private GameObject magicPanel;
-
-        [SerializeField] private GameObject itemPanel;
-
-        [SerializeField] private GameObject limitPanel;
-
-        [SerializeField] private GameObject etcPanel;
-
-        [Header("Misc")] 
-        [SerializeField] private GameObject pageNumber;
-
+        [Header("UI Elements")]
         [SerializeField] private Image panelBackground;
+        
+        private  BattleScreenInput battleScreenInput;
+        private InputAction inputAction;
+
+        private void Awake()
+        {
+            battleScreenInput = new BattleScreenInput();
+            
+            SetCommandPanelVisibility(false);
+        }
+
+        private void OnEnable()
+        {
+            battleScreenInput.Battlescreen.AnyFaceButton.performed += OnFaceButtonPressed;
+            battleScreenInput.Battlescreen.AnyFaceButton.Enable();
+        }
+
+
+        private void OnFaceButtonPressed(InputAction.CallbackContext obj)
+        {
+            SetCommandPanelVisibility(true);
+        }
+
+        private void SetCommandPanelVisibility(bool isVisible)
+        {
+            panelBackground.enabled = isVisible;
+        }
 
     }
 }
